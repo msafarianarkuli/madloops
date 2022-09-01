067a761ce69128c93e8be6ece25237a82415d538
MohammadGlp <mohammadmahdi.gholipour@gmaile.com>
2022-09-01 17:29:26 +0430

    end of landing blog section with responsive

--- src/Components/content/Landing-Blog/landing-blog.component.jsx
+++ src/Components/content/Landing-Blog/landing-blog.component.jsx
@@ -1,5 +1,6 @@
 import { FieldName } from "./../../common/field-name-component/field-name.component";
 import { Button } from "./../../common/button-component/button.component";
+import { BsFillCircleFill } from "react-icons/bs";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper";
 import "swiper/css";
@@ -13,63 +14,68 @@ const LandingBlog = () => {
         <FieldName
           showH2
           showP
-          classH2Field="text-neutral-900 font-bold text-3xl mr-4"
+          classH2Field="text-neutral-900 font-bold sm:text-3xl text-xl mr-4 "
           title="مقالات و خبر ها"
-          classPfield="text-slate-400 mt-2"
+          classPfield="text-slate-400 sm:text-base text-xs mt-2"
           field="با خبر های ما بروز باشید"
         />
       </div>
-      <Button classButton="2xl:absolute 2xl:top-48 2xl:left-16 xl:absolute xl:top-48 xl:left-14 lg:absolute lg:top-52 lg:left-11 z-10 xl:text-xl xl:p-3 md:text-base md:p-2 text-blue-600 outline rounded-xl hover:bg-blue-600 hover:text-white transition ease-in-out delay-150 lg:block sm:hidden">
+      <Button classButton="hidden 2xl:absolute 2xl:top-56 2xl:left-1 xl:absolute xl:top-56 xl:left-0 lg:absolute lg:top-60 lg:left-0 z-10 xl:text-xl xl:p-3 md:text-base md:p-2 text-blue-600 outline rounded-xl hover:bg-blue-600 hover:text-white transition ease-in-out delay-150 lg:block sm:hidden">
         همه مطالب
       </Button>
       <Swiper
         navigation={true}
         modules={[Navigation]}
-        className="w-11/12 sm:mt-6 rounded z-0"
+        className="sm:mt-6 rounded z-0"
       >
         <SwiperSlide>
-          <div className="grid grid-cols-2 lg:mt-48 bg-[#4784DA]">
-            <div className="2xl:w-fit 2xl:absolute 2xl:top-10 2xl:right-18 xl:w-[26rem] 2xl:m-0 xl:m-0 xl:absolute xl:top-16 xl:right-24 lg:m-0 lg:w-[22rem] lg:absolute lg:top-28 lg:right-16 md:w-[14rem] md:mx-16 md:my-6 sm:w-[12rem] rounded-t-xl  bg-slate-400 z-100">
+          <div className="grid grid-cols-2 mt-10 lg:mt-48 bg-[#4784DA]">
+            <div className="xs:w-40 2xl:w-fit 2xl:absolute 2xl:top-10 2xl:right-18 xl:w-[26rem] 2xl:m-0 xl:m-0 xl:absolute xl:top-16 xl:right-24 lg:m-0 lg:w-[22rem] lg:absolute lg:top-28 lg:right-16 md:w-[14rem] md:mx-16 md:my-6 sm:w-[12rem] rounded-t-xl  bg-slate-400 z-100">
               <img
                 src={require("../../../Assets/biden.jpg")}
                 className="lg:p-2 w-full lg:rounded-t-xl scale-100 hover:scale-105 transition duration-500 hover:animate-pulse cursor-pointer"
                 alt=""
               />
             </div>
-            <div className="2xl:mt-20 xl:mt-12 md:mt-10 sm:mt-4 md:mr-6 text-white col-start-2 col-span-4">
-              <div className="flex lg:leading-6 sm:leading-2">
+            <div className="2xl:mt-20 xl:mt-12 md:mt-10 md:mr-6 sm:mt-4 sm:mr-0 mt-5 mr-5  text-white col-start-2 col-span-4">
+              <div className="flex lg:leading-6">
                 <img
                   src={require("../../../Assets/star-fill.svg").default}
-                  className="p-1 lg:w-fit sm:w-6"
+                  className="lg:w-fit lg:p-1 sm:w-4 w-2 mb-[2px] mr-[2px]"
                   alt="star"
                 />
-                <h5 className="md:ml-5 sm:ml-3 lg:text-base sm:text-xs">
+                <h5 className="md:ml-5 lg:text-base sm:text-xs text-[8px] mx-1 ml-3">
                   برترین مقاله
                 </h5>
-                <span className="mb-1">|</span>
-                <div className="md:mr-5 sm:mr-3 flex">
-                  <p className="md:pl-2 sm:pl-1 lg:text-base sm:text-xs">4</p>
-                  <h5 className="lg:text-base sm:text-xs">دقیقه مطالعه</h5>
+                <span className="mb-1 lg:block hidden">|</span>
+                <BsFillCircleFill className="lg:hidden md:w-2 md:mt-[1px] w-[3px]" />
+                <div className="lg:mr-5 md:mr-2 mr-1 flex">
+                  <p className="text-[8px] sm:text-xs pl-1 md:pl-2 lg:text-base ">
+                    4
+                  </p>
+                  <h5 className="lg:text-base sm:text-xs text-[8px]">
+                    دقیقه مطالعه
+                  </h5>
                 </div>
               </div>
-              <Button classButton="w-10/12 text-right font-bold  2xl:mt-8 xl:text-2xl xl:mt-6 lg:mt-4 md:mt-6">
+              <Button classButton="w-10/12 text-right font-bold 2xl:mt-8 xl:text-2xl xl:mt-6 lg:mt-4 md:mt-6 sm:mt-4 mt-2 sm:text-base text-xs">
                 در سال 1401، تیم هکر روسی با حمله به پایگاه داده یکی از شرکت
                 های...
               </Button>
-              <div className="lg:flex 2xl:mt-8 xl:mt-6 lg:mt-4 md:mt-6">
-                <h5 className="2xl:text-base xl:text-sm lg:text-xs md:text-[12px] bg-eye-fill md:bg-[length:12px] md:leading-5 bg-no-repeat bg-right pr-5 lg:ml-4">
+              <div className="lg:flex 2xl:mt-8 xl:mt-6 lg:mt-4 md:mt-6 sm:mt-4 mt-2">
+                <h5 className="2xl:text-base xl:text-sm lg:text-xs sm:text-[12px] text-[10px]  bg-eye-fill lg:bg-[length:16px] bg-[length:10px] sm:bg-[length:12px] lg:leading-6 md:leading-5 bg-no-repeat bg-right pr-5 lg:ml-4">
                   150 بازدید
                 </h5>
-                <span className="lg:block sm:hidden">|</span>
-                <h5 className="2xl:text-base xl:text-sm lg:text-xs md:text-[12px] bg-heart-fill md:bg-[length:12px] md:leading-5 bg-no-repeat bg-right pr-5 lg:mx-4">
+                <span className="lg:block hidden">|</span>
+                <h5 className="2xl:text-base xl:text-sm lg:text-xs sm:text-[12px] text-[10px] bg-heart-fill lg:bg-[length:16px] bg-[length:10px] sm:bg-[length:12px] lg:leading-6 md:leading-5 bg-no-repeat bg-right pr-5 lg:mx-4">
                   25 لایک
                 </h5>
-                <span className="lg:block sm:hidden">|</span>
-                <h5 className="2xl:text-base xl:text-sm lg:text-xs md:text-[12px] bg-comment-fill md:bg-[length:12px] md:leading-5 bg-no-repeat bg-right pr-5 lg:mx-4">
+                <span className="lg:block hidden">|</span>
+                <h5 className="2xl:text-base xl:text-sm lg:text-xs sm:text-[12px] text-[10px] bg-comment-fill lg:bg-[length:16px] bg-[length:10px] sm:bg-[length:12px] lg:leading-6 md:leading-5 bg-no-repeat bg-right pr-5 lg:mx-4">
                   15 نظر
                 </h5>
               </div>
-              <p className="w-10/12 2xl:mt-8 xl:mt-6 lg:mt-4 text-right 2xl:text-base xl:text-sm lg:text-xs lg:block lg:leading-relaxed sm:hidden">
+              <p className="w-10/12 hidden 2xl:mt-8 xl:mt-6 lg:mt-4 text-right 2xl:text-base xl:text-sm lg:text-xs lg:block lg:leading-relaxed sm:hidden">
                 لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
                 استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله
                 در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد
@@ -82,15 +88,16 @@ const LandingBlog = () => {
                 زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات
                 پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
               </p>
-              <Button classButton="xl:text-base sm:text-sm 2xl:my-10 xl:my-8 lg:my-6 md:my-6">
+              <Button classButton="text-[10px] xl:text-base 2xl:my-10 xl:my-8 lg:my-6 md:my-6 sm:text-sm sm:mt-4 mt-2">
                 بیشتر بخوانید...
               </Button>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>salam</SwiperSlide>
+        <SwiperSlide>salam</SwiperSlide>
       </Swiper>
-      <Button classButton="sm:mx-auto sm:mt-10 md:text-base md:p-2 sm:p-1 text-blue-600 outline rounded-xl hover:bg-blue-600 hover:text-white transition ease-in-out delay-150 lg:hidden sm:block">
+      <Button classButton="text-[10px] p-1 sm:mx-auto sm:mt-10 sm:p-1 md:text-base md:p-2 text-blue-600 sm:outline rounded-xl hover:bg-blue-600 hover:text-white transition ease-in-out delay-150 lg:hidden sm:block">
         همه مطالب
       </Button>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,29 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      screens: {
+        " xs": "350px",
+
+        " sm": "640px",
+
+        " md": "768px",
+
+        " lg": "1024px",
+
+        " xl": "1280px",
+
+        "2xl": "1536px",
+      },
       backgroundImage: {
         "eye-fill": "url('Assets/eye-fill-white.svg')",
         "heart-fill": "url('Assets/heart-fill-white.svg')",
         "comment-fill": "url('Assets/chat-left-text-fill-white.svg')",
       },
       colors: {
-        'deep-purple': '#422F7E',
+        "deep-purple": "#422F7E",
       },
     },
   },
