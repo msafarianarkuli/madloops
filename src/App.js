947c119fc19a1a23a5850ae39ec9fe821e18247d
Mikinem <michaelmohseni59@gmail.com>
2022-09-05 03:46:55 +0430

    fix teacher slider

--- src/App.js
+++ src/App.js
@@ -2,21 +2,15 @@ import "./App.css";
 import Navigation from "../src/Components/Navigation/navigation.component";
 import LandingBlog from "./Components/content/Landing-Blog/landing-blog.component";
 import LandingHeader from "./Components/content/Landing-Header/landing-header.component";
-=======
 import LandingPage from "./Screens/Landing/landing-page.component";
 import BlogsPage from "./Screens/Blogs/blogs-page.component";
->>>>>>> 5ddc7e819bc5ef0bc6d6396df3d084fe65cd78bc
+import LandingTeacher from "./Components/content/Landing-Teacher/teacher.component";
 
 const App = () => {
   return (
     <div className="App">
       <Navigation />
-<<<<<<< HEAD
-      <LandingHeader/>
-=======
-      {/*  <BlogsPage />*/}
-      <LandingPage />
->>>>>>> 5ddc7e819bc5ef0bc6d6396df3d084fe65cd78bc
+      <LandingTeacher/>
     </div>
   );
 };
