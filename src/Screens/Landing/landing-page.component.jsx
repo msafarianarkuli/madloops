cb4df22385d1219d6b74e9db0834c7ae6f6301b7
MohammadGlp <mohammadmahdi.gholipour@gmaile.com>
2022-09-07 00:26:13 +0430

    slight change in landing section

--- src/Screens/Landing/landing-page.component.jsx
+++ src/Screens/Landing/landing-page.component.jsx
@@ -1,10 +1,18 @@
 import LandingBlog from "../../Components/content/Landing-Blog/landing-blog.component";
 import LandingTeacher from "../../Components/content/Landing-Teacher/teacher.component";
+import LandingServices from "./../../Components/content/Landing-Services/landing-services.component";
+import LandingHeader from "./../../Components/content/Landing-Header/landing-header.component";
+import LandingOffers from "./../../Components/content/Landing-Offers/landing-offers.component";
+import LandingCource from "./../../Components/content/Landing-Course/landing-cource.component";
 
 const LandingPage = () => {
   return (
     <>
+      <LandingHeader />
+      <LandingServices />
+      <LandingCource />
       <LandingTeacher />
+      <LandingOffers />
       <LandingBlog />
     </>
   );
