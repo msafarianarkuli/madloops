166991555a030a250ac1f5da82bbfcb1843eb50d
Mikinem <michaelmohseni59@gmail.com>
2022-09-13 17:14:01 +0430

    develop single blog

--- src/Core/services/Fake Service/SingleBlogSuggestion.js
+++ src/Core/services/Fake Service/SingleBlogSuggestion.js
@@ -7,64 +7,64 @@ const Data = {
       title: "ری اکت",
       descibtion:
         "محمد تقی بعد از نمونه ازمایش های پی در پی توانست بر امریکا قلبه کند و با بایدن گلف بازی کند",
-      date: "2011-09-11",
+      date: "21 تیر 1401",
       img: require("../../../Assets/img/react-hooks.png"),
+    },
+    {
+      id: uuidv4(),
+      title: "سی اس اس",
+      descibtion:
+        "محمد تقی بعد از نمونه ازمایش های پی در پی توانست بر امریکا قلبه کند و با بایدن گلف بازی کند",
+      date: "21 تیر 1401",
+      img: require("../../../Assets/img/cssback.jpg"),
+    },
+    {
+      id: uuidv4(),
+      title: "اچ تی ام ال",
+      descibtion:
+        "محمد تقی بعد از نمونه ازمایش های پی در پی توانست بر امریکا قلبه کند و با بایدن گلف بازی کند",
+      date: "21 تیر 1401",
+      img: require("../../../Assets/img/htmlback.jpg"),
+    },
+    {
+      id: uuidv4(),
+      title: "انگولار",
+      descibtion:
+        "محمد تقی بعد از نمونه ازمایش های پی در پی توانست بر امریکا قلبه کند و با بایدن گلف بازی کند",
+      date: "21 تیر 1401",
+      img: require("../../../Assets/img/maxresdefault.jpg"),
     },
     {
       id: uuidv4(),
       title: "ری اکت",
       descibtion:
         "محمد تقی بعد از نمونه ازمایش های پی در پی توانست بر امریکا قلبه کند و با بایدن گلف بازی کند",
-      date: "2011-09-11",
+      date: "21 تیر 1401",
       img: require("../../../Assets/img/react-hooks.png"),
     },
     {
       id: uuidv4(),
-      title: "ری اکت",
+      title: "نود جی اس",
       descibtion:
         "محمد تقی بعد از نمونه ازمایش های پی در پی توانست بر امریکا قلبه کند و با بایدن گلف بازی کند",
-      date: "2011-09-11",
-      img: require("../../../Assets/img/react-hooks.png"),
+      date: "21 تیر 1401",
+      img: require("../../../Assets/img/nodejsback.png"),
     },
     {
       id: uuidv4(),
-      title: "ری اکت",
+      title: "سی اس اس",
       descibtion:
         "محمد تقی بعد از نمونه ازمایش های پی در پی توانست بر امریکا قلبه کند و با بایدن گلف بازی کند",
-      date: "2011-09-11",
-      img: require("../../../Assets/img/react-hooks.png"),
+      date: "21 تیر 1401",
+      img: require("../../../Assets/img/cssback.jpg"),
     },
     {
       id: uuidv4(),
-      title: "ری اکت",
+      title: "اچ تی ام ال",
       descibtion:
         "محمد تقی بعد از نمونه ازمایش های پی در پی توانست بر امریکا قلبه کند و با بایدن گلف بازی کند",
-      date: "2011-09-11",
-      img: require("../../../Assets/img/react-hooks.png"),
-    },
-    {
-      id: uuidv4(),
-      title: "ری اکت",
-      descibtion:
-        "محمد تقی بعد از نمونه ازمایش های پی در پی توانست بر امریکا قلبه کند و با بایدن گلف بازی کند",
-      date: "2011-09-11",
-      img: require("../../../Assets/img/react-hooks.png"),
-    },
-    {
-      id: uuidv4(),
-      title: "ری اکت",
-      descibtion:
-        "محمد تقی بعد از نمونه ازمایش های پی در پی توانست بر امریکا قلبه کند و با بایدن گلف بازی کند",
-      date: "2011-09-11",
-      img: require("../../../Assets/img/react-hooks.png"),
-    },
-    {
-      id: uuidv4(),
-      title: "ری اکت",
-      descibtion:
-        "محمد تقی بعد از نمونه ازمایش های پی در پی توانست بر امریکا قلبه کند و با بایدن گلف بازی کند",
-      date: "2011-09-11",
-      img: require("../../../Assets/img/react-hooks.png"),
+      date: "21 تیر 1401",
+      img: require("../../../Assets/img/htmlback.jpg"),
     },
   ],
 };
