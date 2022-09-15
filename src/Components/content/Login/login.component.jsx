2a5ed5e00d070f2e80e7dff22b6fc1d5d0aa5d42
Mikinem <michaelmohseni59@gmail.com>
2022-09-15 17:04:37 +0430

    add login page

--- src/Components/content/Login/login.component.jsx
+++ src/Components/content/Login/login.component.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Login = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className="container m-auto">
+      <div className="grid grid-cols-2 h-screen">
+        <div className="col-span-1 bg-lite-gray relative">
+          <div className="h-650 bg-red-200"></div>
+          <div className="h-20 w-full bg-red-500 absolute bottom-0">a</div>
+        </div>
+        <div className="col-span-1 bg-white">
+          <div className="h-56 bg-blue-300"></div>
+          <div className="h-[487px] bg-blue-500"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
