095a371bc1d340d0e46923c7e9c5d3c5801f693b
Mikinem <michaelmohseni59@gmail.com>
2022-09-16 14:38:39 +0430

    develop login

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
-import Navigation from '../src/Components/Navigation/navigation.component';
-import LandingPage from './Screens/Landing/landing-page.component';
-import BlogsPage from './Screens/Blogs/blogs-page.component';
-import SingleBlog from './Screens/Single-Blog/single-blog.component';
-import CoursesPage from './Screens/Courses/courses-page.component';
-import ContactUs from './Screens/ContactUs/ContactUs';
-import SingleCourse from './Screens/SingleCourse/SingleCourse';
-import Footer from './Components/Footer/footer.component';
-import NotFoundPage from './Screens/Not-Found/not-found.component';
-import Call from './Components/content/ContactUs/Call';
-import SendMessage from './Components/content/ContactUs/SendMessage';
-import Location from './Components/content/ContactUs/Location';
-import UserPanel from './Screens/Panel/UserPanel';
-import Dashboard from './Components/content/Panel/Dashboard';
-import MyCourses from './Components/content/Panel/MyCourses';
-import CoursesList from './Components/content/Panel/CoursesList';
-import EditProfile from './Components/content/Panel/EditProfile';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navigation from "../src/Components/Navigation/navigation.component";
+import LandingPage from "./Screens/Landing/landing-page.component";
+import BlogsPage from "./Screens/Blogs/blogs-page.component";
+import SingleBlog from "./Screens/Single-Blog/single-blog.component";
+import CoursesPage from "./Screens/Courses/courses-page.component";
+import ContactUs from "./Screens/ContactUs/ContactUs";
+import SingleCourse from "./Screens/SingleCourse/SingleCourse";
+import Footer from "./Components/Footer/footer.component";
+import NotFoundPage from "./Screens/Not-Found/not-found.component";
+import Call from "./Components/content/ContactUs/Call";
+import SendMessage from "./Components/content/ContactUs/SendMessage";
+import Location from "./Components/content/ContactUs/Location";
+import LoginPage from "./Screens/Login/login.component";
+import ForgetPasswordPage from "./Screens/Forget-Password/forget-password.component";
+import SignUpPage from "./Screens/Sign-up/sign-up.component";
+
 const App = () => {
   return (
     <div className="App">
@@ -34,24 +33,15 @@ const App = () => {
           <Route path="single-blog" element={<SingleBlog />} />
           <Route path="login" element={<LoginPage />} />
           <Route path="sign-up" element={<SignUpPage />} />
-          <Route
-            path="forget-pass"
-            element={<ForgetPasswordPage />}
-          />
+          <Route path="forget-pass" element={<ForgetPasswordPage />} />
 
           {/*
           <Route path="single-blog" element={} />
           <Route path="login" element={<LoginPage />} />
           <Route path="register" element={<RegisterPage />} />
           <Route path="forget-pass" element={<ForgetPassPage />} />
-          
+          <Route path="user-panel/*" element={<UserDashboard />} />
           */}
-          <Route path="user-panel" element={<UserPanel />}>
-            <Route index element={<Dashboard />}></Route>
-            <Route path="myCourses" element={<MyCourses />} />
-            <Route path="courseList" element={<CoursesList />} />
-            <Route path="editProfile" element={<EditProfile />} />
-          </Route>
           <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
