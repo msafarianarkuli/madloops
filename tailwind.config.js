f5753753059bdae9b7266e639e973a9569a9d290
Mikinem <michaelmohseni59@gmail.com>
2022-09-18 17:01:27 +0430

    fix button

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,41 +1,41 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       screens: {
-        ' xs': '350px',
+        " xs": "350px",
 
-        ' sm': '640px',
+        " sm": "640px",
 
-        ' md': '768px',
+        " md": "768px",
 
-        ' lg': '1024px',
+        " lg": "1024px",
 
-        ' xl': '1280px',
+        " xl": "1280px",
 
-        '2xl': '1536px',
+        "2xl": "1536px",
       },
       backgroundImage: {
-        'eye-fill': "url('Assets/eye-fill-white.svg')",
-        'heart-fill': "url('Assets/heart-fill-white.svg')",
-        'comment-fill': "url('Assets/chat-left-text-fill-white.svg')",
-        'lines-dash': "url('Assets/img/lines-dash.svg')",
+        "eye-fill": "url('Assets/eye-fill-white.svg')",
+        "heart-fill": "url('Assets/heart-fill-white.svg')",
+        "comment-fill": "url('Assets/chat-left-text-fill-white.svg')",
+        "lines-dash": "url('Assets/img/lines-dash.svg')",
       },
       height: {
-        "100": "100px",
-        "200": "200px",
-        "300": "300px",
-        "400": "400px",
-        "450": "450px",
-        "500": "500px",
-        "550": "550px",
-        "600": "600px",
-        "650": "650px",
-        "700": "700px",
-        "750": "750px",
-        "800": "800px",
+        100: "100px",
+        200: "200px",
+        300: "300px",
+        400: "400px",
+        450: "450px",
+        500: "500px",
+        550: "550px",
+        600: "600px",
+        650: "650px",
+        700: "700px",
+        750: "750px",
+        800: "800px",
       },
       colors: {
         "deep-purple": "#422F7E",
@@ -50,17 +50,17 @@ module.exports = {
         "cool-gray": "#555555",
       },
       colors: {
-        'deep-purple': '#422F7E',
-        'lite-purple': '#7F56DA',
-        'button-purple': '#7A58C9',
-        'dark-purple': '#756496',
-        'lite-pink': '#F4EBFD',
-        'lite-white': '#F9F9FF',
-        'lite-gray': '#F3F4F8',
-        'deep-gray': '#8D8D8D',
-        'sick-gray': '#B7B7B7',
-        'cool-gray': '#555555',
-        'lite-violet': '#D1C1F6',
+        "deep-purple": "#422F7E",
+        "lite-purple": "#7F56DA",
+        "button-purple": "#7A58C9",
+        "dark-purple": "#756496",
+        "lite-pink": "#F4EBFD",
+        "lite-white": "#F9F9FF",
+        "lite-gray": "#F3F4F8",
+        "deep-gray": "#8D8D8D",
+        "sick-gray": "#B7B7B7",
+        "cool-gray": "#555555",
+        "lite-violet": "#D1C1F6",
       },
       keyframes: {
         onLoadAnim: {
@@ -77,8 +77,6 @@ module.exports = {
           "0%": { transform: "translateY(0)" },
           "50%": { transform: "translateY(-2px)" },
           "100%": { transform: "translateY(0)" },
-          '0%': { transform: 'translateX(-300px)' },
-          '100%': { transform: 'translateX(0)' },
         },
       },
       animation: {
@@ -87,7 +85,7 @@ module.exports = {
         onHoverGoogle: "onHoverGoogle 1s ease-in-out infinite",
       },
       borderRadius: {
-        '5xl': '41px',
+        "5xl": "41px",
       },
     },
   },
