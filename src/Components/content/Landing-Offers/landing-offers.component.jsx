686aff4f5c101d6da8b2d690f6c749c0f68b7a00
Mikinem <michaelmohseni59@gmail.com>
2022-10-01 23:15:33 +0330

    fix landing offer form

--- src/Components/content/Landing-Offers/landing-offers.component.jsx
+++ src/Components/content/Landing-Offers/landing-offers.component.jsx
@@ -14,12 +14,12 @@ const LandingOffers = () => {
           >
             می توانید در باکس زیر پیشنهاد خود را وارد کنید
           </LandingTitle>
-          <div className="bg-gray-300 grid grid-cols-1 md:grid-cols-2 pt-5">
+          <div className="bg-gray-300 grid grid-cols-1 md:grid-cols-2">
             <div className="flex justify-center items-center">
               <OffersForm />
             </div>
             <div className="hidden md:block">
-              <img className="w-96 pt-2" src={offers} />
+              <img className="w-96 pt-11" src={offers} />
             </div>
           </div>
         </div>
