b39b2da293e0543ada8c4c14f6c82bb266e21df0
Mikinem <michaelmohseni59@gmail.com>
2022-10-03 18:30:37 +0330

    edit contact us

--- src/Components/content/ContactUs/Call.jsx
+++ src/Components/content/ContactUs/Call.jsx
@@ -4,28 +4,28 @@ import { BsEnvelopeFill, BsTelephoneFill } from 'react-icons/bs';
 const Call = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-4 py-12 text-lg">
-      <div className="flex justify-evenly  bg-lite-purple rounded-2xl py-14 lg:mx-10">
-        <div className="flex items-center text-white">
+      <div className="flex justify-evenly border-2 border-lite-purple rounded-lg py-14 lg:mx-10">
+        <div className="flex items-center text-lite-purple">
           <BsTelephoneFill className="text-3xl mb-3 ml-3" />
           <p className="hidden sm:block">تلفن تماس</p>
         </div>
         <div>
-          <p className="text-white">
+          <p className="text-lite-purple">
             <bdo dir="ltr">+9801133221144</bdo>
           </p>
-          <p className="text-white">
+          <p className="text-lite-purple">
             <bdo dir="ltr">+9801133221144</bdo>
           </p>
         </div>
       </div>
-      <div className="flex justify-evenly items-center bg-lite-purple rounded-2xl py-14 lg:mx-10 sm:px-5 xl:px-0">
-        <div className="flex items-center text-white">
-          <BsEnvelopeFill className="text-3xl text-white mt-2 mb-3 ml-3" />
+      <div className="flex justify-evenly items-center border-2 border-lite-purple rounded-lg py-14 lg:mx-10 sm:px-5 xl:px-0">
+        <div className="flex items-center text-lite-purple">
+          <BsEnvelopeFill className="text-3xl text-lite-purple mt-2 mb-3 ml-3" />
           <p className="hidden sm:block">ایمیل پژوهشگاه</p>
         </div>
         <div className="text-left">
-          <p className="text-white mb-2">Sepehr.Sari@gmail.com</p>
-          <p className="text-white">Bahr.academy@gmail.com</p>
+          <p className="text-lite-purple mb-2">Sepehr.Sari@gmail.com</p>
+          <p className="text-lite-purple">Bahr.academy@gmail.com</p>
         </div>
       </div>
     </div>
--- src/Components/content/ContactUs/ContactItem.jsx
+++ src/Components/content/ContactUs/ContactItem.jsx
@@ -8,7 +8,7 @@ const ContactItem = ({ icon, title, to }) => {
       <div
         className={`${
           location.pathname === to ? 'bg-lite-purple' : ''
-        } flex flex-col justify-center items-center m-auto rounded-2xl py-3 sm:py-6 lg:py-8 lg:w-52 sm:w-36 w-24
+        } flex flex-col justify-center items-center m-auto rounded-lg py-3 sm:py-6 lg:py-8 lg:w-52 sm:w-36 w-24
     hover:bg-lite-purple transition ease-in duration-200 cursor-pointer parent-hover:text-white group`}
       >
         <span
