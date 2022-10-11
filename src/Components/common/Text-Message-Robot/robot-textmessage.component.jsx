0ff25a58b36290d17dd7bfdcafaeda0bbcb56ab5
Mikinem <michaelmohseni59@gmail.com>
2022-10-12 03:13:23 +0330

    fix robot

--- src/Components/common/Text-Message-Robot/robot-textmessage.component.jsx
+++ src/Components/common/Text-Message-Robot/robot-textmessage.component.jsx
@@ -9,10 +9,10 @@ const RobotTextMessage = () => {
       <div className="lg:w-60 w-52 absolute top-[-400px] bg-lite-violet dark:bg-[#5DC8B2] dark:text-dark-primary p-2 pr-5 z-20 rounded-t-full rounded-r-full animate-[onLoadTextTwo_7s_ease-in-out] duration-300">
         <h2 className="lg:text-xl text-lg">اینجا هر دوره ای بخوای هست</h2>
       </div>
-      <div className="lg:w-60 w-52 absolute top-[-400px] bg-lite-violet dark:bg-[#5DC8B2] dark:text-dark-primary p-2 pr-5 z-30 rounded-t-full rounded-r-full animate-[onLoadTextThree_9s_ease-in-out] duration-300">
+      <div className="lg:w-60 w-52 absolute top-[-400px] bg-lite-violet dark:bg-[#5DC8B2] dark:text-dark-primary p-2 pr-5 z-30 rounded-t-full rounded-r-full animate-[onLoadTextThree_9.4s_ease-in-out] duration-300">
         <h2 className="lg:text-xl text-lg">با خبر های روز تکنولوژی دنیا</h2>
       </div>
-      <div className="lg:w-72 w-64 absolute top-[-400px] bg-lite-violet dark:bg-[#5DC8B2] dark:text-dark-primary p-2 pr-4 z-40 rounded-t-3xl -right-12 rounded-r-3xl animate-[onLoadTextFour_13s_ease-in-out] duration-300">
+      <div className="lg:w-72 w-64 absolute top-[-400px] bg-lite-violet dark:bg-[#5DC8B2] dark:text-dark-primary p-2 pr-4 z-40 rounded-t-3xl -right-12 rounded-r-3xl animate-[onLoadTextFour_13.4s_ease-in-out] duration-300">
         <h2 className="lg:text-xl text-lg">اگه سوالی داشتی میتونی تو چت آنلاین با ما به اشتراک بزاری</h2>
       </div>
     </div>
