c7a5174de299d273188d483cc6895d7b4695d909
MohammadGlp <mohammadmahdi.gholipour@gmaile.com>
2022-11-03 11:01:45 +0330

    save x token

--- src/Core/services/api/apiSlice.js
+++ src/Core/services/api/apiSlice.js
@@ -4,6 +4,16 @@ export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
     baseUrl: "https://api.madloops.sepehracademy.ir/api/",
+    prepareHeaders: (headers, { getState }) => {
+      // By default, if we have a token in the store, let's use that for authenticated requests
+      const token = getState().auth.token;
+
+      if (token) {
+        headers.set("x-auth-token", `${token}`);
+      }
+      return headers;
+    },
   }),
+
   endpoints: (builder) => ({}),
 });
