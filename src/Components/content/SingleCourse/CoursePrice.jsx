657e2039b8105bad0136225cf8480a444714511d
MohammadGlp <mohammadmahdi.gholipour@gmaile.com>
2022-11-17 18:22:19 +0330

    ...

--- src/Components/content/SingleCourse/CoursePrice.jsx
+++ src/Components/content/SingleCourse/CoursePrice.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState } from "react";
 import { FaCoins } from "react-icons/fa";
 import { TbDiscount2 } from "react-icons/tb";
 import { useDispatch } from "react-redux";
@@ -8,20 +8,11 @@ import Modal from "../../common/Modal/modal.component";
 import { Button } from "./../../common/button-component/button.component";
 
 const CoursePrice = ({ item }) => {
-  const future = new Date(item?.startDate).getTime();
-  // console.log(future);
-  const futureTime = new Date(future + 86400000 * 5).getTime();
-  // console.log(futureTime);
-  // console.log(Date.now() - futureTime);
   const [showModal, setShowModal] = useState(false);
   const [showEModal, setEShowModal] = useState(false);
-  const [minTimer, setMinTimre] = useState({});
-  const [time, setTime] = useState();
-  // new Date(new Date(item?.startDate).getTime() + 5000).getTime()
-  // console.log(time);
-  const [stop, setStop] = useState(false);
+
   const dispatch = useDispatch();
-  const intervalRef = useRef(null);
+
   const addProductToCart = () => dispatch(addItem(item));
   const addProductForSave = () => dispatch(addBookMark(item));
 
@@ -33,52 +24,6 @@ const CoursePrice = ({ item }) => {
     setEShowModal((prev) => !prev);
   };
 
-  // useEffect(() => {
-  //   setTime(Date.now() - futureTime);
-  // }, [item?.startDate]);
-
-  useEffect(() => {
-    setInterval(() => {
-      setTime(Date.now() - futureTime);
-      console.log(getReturnValues(time));
-    }, 1000);
-  }, []);
-
-  // useEffect(() => {
-  //   if (intervalRef.current) {
-  //     clearInterval(intervalRef.current);
-  //     intervalRef.current = null;
-  //     return;
-  //   }
-
-  //   const interval = (intervalRef.current = setInterval(() => {
-  //     setTime(
-  //       new Date(new Date(item?.startDate).getTime() + 86400000 * 5).getTime() -
-  //         Date.now()
-  //       // new Date(item?.startDate).getTime()
-  //     );
-  //   }, 1000));
-
-  //   return () => clearInterval(interval);
-  // }, [stop]);
-
-  // if (minTimer?.seconds <= 0) {
-  //   setStop(true);
-  // }
-  const getReturnValues = (countDown) => {
-    // console.log(date);
-    // const countDown = new Date(date).getTime() - Date.now();
-    // console.log(countDown);
-    const days = Math.floor(countDown / (1000 * 60 * 60 * 24));
-    const hours = Math.floor(
-      (countDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    const minutes = Math.floor((countDown % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((countDown % (1000 * 60)) / 1000);
-    // setMinTimre({ seconds });
-    return [days, hours, minutes, seconds];
-  };
-  // console.log(getReturnValues(time));
   return (
     <div className="course-Detail-container">
       <div className="course-detail-title-box">
